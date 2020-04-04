72cc3c80a338b0ce9c46d8647069a2c07e003b6e
xiapeimin <2536733090@qq.com>
2020-04-04 16:22:01 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,18 +46,6 @@ module.exports = function (grunt) {
     },
     cssmin: {
       'dist/bundle.min.css': './css/index.css'
-    },
-    useminPrepare: {
-      html: 'index.html',
-      options: {
-        dest: 'dist'
-      }
-    },
-    usemin: {
-      html: ['dist/index.html']
-    },
-    clean: {
-      end: ['dist/bundle.css', 'dist/bundle.js', '.tmp']
     }
   });
 
@@ -69,10 +57,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-usemin');
 
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
-  grunt.registerTask('build', ['copy:html', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin', 'htmlmin', 'clean:end']);
+  grunt.registerTask('build', ['copy:html', 'concat', 'uglify', 'cssmin',  'htmlmin']);
 };
